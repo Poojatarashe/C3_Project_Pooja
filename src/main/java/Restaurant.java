435b26fb4d82d7570169d5e677f0e492d790f52b
Poojatarashe <poojatarashe1996@gmail.com>
2021-04-12 23:44:52 +0530

    Part2: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -71,23 +71,7 @@ public class Restaurant {
         return name;
     }
 
-
-
-ArrayList<String> itemNames = new ArrayList<>();
-    public int calculateOrderValue(List<String> itemNames){
-
-         List<Item> items = new ArrayList<>();
-         int sumOfPrice = 0;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
-        for (Item item: items) {
-            if (item.getName().equals(itemNames)) {
-                return item.getPrice();
-            }
-            sumOfPrice += item.getPrice();
-
-        }
-        return sumOfPrice;
-    }
+   
 
 }
 
